f1492121606394c03f824e92fa157f1871a3a171
Cecilia Olejarczyk <chechuolejarczyk@gmail.com>
2023-09-08 20:50:39 -0300

    Primera subida

---
+++ universidadgrupo93/UniversidadGruppo93/src/universidadgruppo93/Entidades/Alumno.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package universidadgruppo93.Entidades;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author aldio
+ */
+public class Alumno {
+    private int idAlumno;
+    private String apellido;
+    private String nombre;
+    private LocalDate fechaNac;
+    private boolean activo;
+
+    public Alumno() {
+    }
+
+    public Alumno(int idAlumno, String apellido, String nombre, LocalDate fechaNac, boolean activo) {
+        this.idAlumno = idAlumno;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.fechaNac = fechaNac;
+        this.activo = activo;
+    }
+
+    public Alumno(String apellido, String nombre, LocalDate fechaNac, boolean activo) {
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.fechaNac = fechaNac;
+        this.activo = activo;
+    }
+
+    public int getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(int idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(LocalDate fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" + "idAlumno=" + idAlumno + ", apellido=" + apellido + ", nombre=" + nombre + ", fechaNac=" + fechaNac + '}';
+    }
+    
+    
+}
---
+++ universidadgrupo93/UniversidadGruppo93/src/universidadgruppo93/Entidades/materia.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package universidadgruppo93.Entidades;
+
+/**
+ *
+ * @author aldio
+ */
+public class materia {
+    
+}
