b5e32f0c2251258d5aad0747a9563be05a7014b6
albert2023java <141685062+albert2023java@users.noreply.github.com>
2023-09-11 19:30:01 -0300

    entidades formuladas. faltan vistas y conexion.

--- universidadgrupo93/UniversidadGruppo93/src/universidadgruppo93/Entidades/Inscripcion.java
+++ universidadgrupo93/UniversidadGruppo93/src/universidadgruppo93/Entidades/Inscripcion.java
@@ -11,4 +11,63 @@ package universidadgruppo93.Entidades;
  */
 public class Inscripcion {
     
+     private int idInscripto;
+    private double nota;
+    private int idMateria;
+    private int idAlumno;
+
+    public Inscripcion() {
+    }
+
+    public Inscripcion(int idInscripto, double nota, int idMateria, int idAlumno) {
+        this.idInscripto = idInscripto;
+        this.nota = nota;
+        this.idMateria = idMateria;
+        this.idAlumno = idAlumno;
+    }
+
+    public Inscripcion(double nota, int idMateria, int idAlumno) {
+        this.nota = nota;
+        this.idMateria = idMateria;
+        this.idAlumno = idAlumno;
+    }
+
+    public int getIdInscripto() {
+        return idInscripto;
+    }
+
+    public void setIdInscripto(int idInscripto) {
+        this.idInscripto = idInscripto;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public int getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(int idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    @Override
+    public String toString() {
+        return "Inscripcion{" + "idInscripto=" + idInscripto + ", nota=" + nota + ", idMateria=" + idMateria + ", idAlumno=" + idAlumno + '}';
+    }
+    
+    
 }
