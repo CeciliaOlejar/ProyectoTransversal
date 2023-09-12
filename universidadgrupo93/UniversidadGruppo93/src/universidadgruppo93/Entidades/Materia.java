0facef41bc1fd5bd3e86bf17fa2c77fd473ed8b5
GabiiR <grgithub@gmail.com>
2023-09-11 21:14:28 -0300

    Update Materia.java

---
+++ universidadgrupo93/UniversidadGruppo93/src/universidadgruppo93/Entidades/Materia.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package universidadgruppo93.Entidades;
+
+/**
+ *
+ * @author aldio
+ */
+public class Materia {
+    
+    private int idMateria;
+    private String nombre;
+    private int anioMateria;
+    private boolean activo;
+
+    public Materia() {
+    }
+
+    public Materia(int idMateria, String nombre, int anioMateria, boolean activo) {
+        this.idMateria = idMateria;
+        this.nombre = nombre;
+        this.anioMateria = anioMateria;
+        this.activo = activo;
+    }
+
+    public Materia(String nombre, int anioMateria, boolean activo) {
+        this.nombre = nombre;
+        this.anioMateria = anioMateria;
+        this.activo = activo;
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAnioMateria() {
+        return anioMateria;
+    }
+
+    public void setAnioMateria(int anioMateria) {
+        this.anioMateria = anioMateria;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    @Override
+    public String toString() {
+        return "materia{" + "idMateria=" + idMateria + ", nombre=" + nombre + ", anioMateria=" + anioMateria + ", activo=" + activo + '}';
+    }
+}
