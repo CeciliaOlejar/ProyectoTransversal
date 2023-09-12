ff526cf25e7cbef390b13ef7ffbb4df0d41dbb71
GabiiR <grgithub@gmail.com>
2023-09-11 21:05:18 -0300

    Update Conexion.java

--- universidadgrupo93/UniversidadGruppo93/src/universidadgruppo93/AccesoADatos/Conexion.java
+++ universidadgrupo93/UniversidadGruppo93/src/universidadgruppo93/AccesoADatos/Conexion.java
@@ -1,12 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package universidadgruppo93.AccesoADatos;
 
+import java.sql.Connection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 public class Conexion {
-
+    //localhost = Esta pc.
+    private static final String URL = "jdbc:mariadb://localhost/"; //Static = Constante.
+    
+    //Nombre base de datos:
+    private static final String DB = "UniversidadULPGrupo93";
+    
+    //Usuario "root" es el default.
+    private static final String USUARIO = "root";
+    
+    //Contraseña.
+    private static final String PASSWORD = "";
+    /*------------------------------------------*/
+    private static Connection connection;
+    
+    private Conexion (){}; //Constructor.
+        
+    public static Connection getConexion(){
+        
+        if (connection == null) {
+            
+            try {
+                Class.forName("org.mariadb.jdbc.Driver"); //Drivers de libreria.
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "Error al carga el driver de conexión.");
+            }
+            
+        }
+        return null;
+    }
+    
 }
