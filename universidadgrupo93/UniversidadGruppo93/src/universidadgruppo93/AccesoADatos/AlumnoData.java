e18e44046ec7dde41519675c376d662b5dd9b77d
GabiiR <grgithub@gmail.com>
2023-09-11 23:46:06 -0300

    Se agrego la clase AlumnoData

---
+++ universidadgrupo93/UniversidadGruppo93/src/universidadgruppo93/AccesoADatos/AlumnoData.java
@@ -0,0 +1,13 @@
+package universidadgruppo93.AccesoADatos;
+
+import java.sql.Connection;
+import universidadgruppo93.Entidades.Alumno;
+
+public class AlumnoData {
+    private Connection con;
+
+    public AlumnoData() {
+    }
+
+    public void guardarAlumno(Alumno alumno) {};
+}
