0e01375d51e86721de4bc08c54dad8b702efe286
GabiiR <grgithub@gmail.com>
2023-09-18 19:13:17 -0300

    Update InscripcionData.java

--- universidadgrupo93/UniversidadGruppo93/src/universidadgruppo93/AccesoADatos/InscripcionData.java
+++ universidadgrupo93/UniversidadGruppo93/src/universidadgruppo93/AccesoADatos/InscripcionData.java
@@ -1,6 +1,14 @@
-
 package universidadgruppo93.AccesoADatos;
 
+import java.sql.Connection;
+
 public class InscripcionData {
-    
-}
+
+    private Connection con = null; //Se agrega una conexión.
+
+    public InscripcionData() {
+
+        this.con = Conexion.getConexion();
+        
+    }
+};
