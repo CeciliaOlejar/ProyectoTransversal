5c95a25d263cf6d1ba703f017348f2d5389d2273
GabiiR <grgithub@gmail.com>
2023-09-21 20:53:44 -0300

    Update UniversidadGruppo93.java

--- universidadgrupo93/UniversidadGruppo93/src/universidadgruppo93/UniversidadGruppo93.java
+++ universidadgrupo93/UniversidadGruppo93/src/universidadgruppo93/UniversidadGruppo93.java
@@ -2,9 +2,11 @@ package universidadgruppo93;
 
 import java.time.LocalDate;
 import universidadgruppo93.AccesoADatos.AlumnoData;
+import universidadgruppo93.AccesoADatos.InscripcionData;
 import universidadgruppo93.Entidades.Alumno;
 import universidadgruppo93.Entidades.Materia;
 import universidadgruppo93.AccesoADatos.MateriaData;
+import universidadgruppo93.Entidades.Inscripcion;
 
 /**
  *
@@ -14,22 +16,33 @@ public class UniversidadGruppo93 {
 
     public static void main(String[] args) {
         //Connection con = Conexion.getConexion();
+        /*-----------------------------------------*/
         //Alumno luis=new Alumno("Sosa", "Julio", 24332743, LocalDate.of(1999, 4, 12), true);
-//        AlumnoData alu = new AlumnoData();
+        //AlumnoData alu = new AlumnoData();
         //alu.guardarAlumno(luis);
         //alu.modificarAlumno(luis);
         //alu.eliminarAlumno(7);
-//        Alumno alumnoEncontrado= alu.buscarAlumnoPorDni(24332743);
-//        System.out.println("dni: "+alumnoEncontrado.getDni());
-//        System.out.println("Apellido: "+ alumnoEncontrado.getApellido());
-//        System.out.println("Nombre: "+ alumnoEncontrado.getNombre());
-        Materia materia1 = new Materia("HISTORIA", 9, true);
-        MateriaData md1 = new MateriaData();
-        md1.guardarMateria(materia1);
-//          md1.buscarMateria(1);
-//          md1.modificarMateria(materia1);
-//        md1.eliminarMateria(materia1);
-        md1.listarMateria(materia1);
-
+        //Alumno alumnoEncontrado= alu.buscarAlumnoPorDni(24332743);
+        //System.out.println("dni: "+alumnoEncontrado.getDni());
+        //System.out.println("Apellido: "+ alumnoEncontrado.getApellido());
+        //System.out.println("Nombre: "+ alumnoEncontrado.getNombre());
+        /*-----------------------------------------*/
+        //Materia materia1 = new Materia("HISTORIA", 9, true);
+        //MateriaData md1 = new MateriaData();
+        //md1.guardarMateria(materia1);
+        //md1.buscarMateria(1);
+        //md1.modificarMateria(materia1);
+        //md1.eliminarMateria(materia1);
+        
+        //md1.listarMateria(materia1);
+        /*-----------------------------------------*/
+        
+        //Alumno alumno = new Alumno();
+        //AlumnoData alumno1 = new AlumnoData();
+        //alumno1.listarAlumnos(alumno);
+        /*-----------------------------------------*/
+        Inscripcion insc = new Inscripcion();
+        InscripcionData insc1 = new InscripcionData();
+        insc1.guardarInscripcion(insc);
     }
 }
