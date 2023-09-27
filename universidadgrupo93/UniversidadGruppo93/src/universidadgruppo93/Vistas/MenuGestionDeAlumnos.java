d852e09869ee02081fb47982a90b1c4d01b3ec18
Cecilia Olejarczyk <chechuolejarczyk@gmail.com>
2023-09-27 18:57:12 -0300

    borro comentarios

--- universidadgrupo93/UniversidadGruppo93/src/universidadgruppo93/Vistas/MenuGestionDeAlumnos.java
+++ universidadgrupo93/UniversidadGruppo93/src/universidadgruppo93/Vistas/MenuGestionDeAlumnos.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package universidadgruppo93.Vistas;
 
 import java.time.LocalDate;
@@ -12,10 +7,6 @@ import javax.swing.JOptionPane;
 import universidadgruppo93.AccesoADatos.AlumnoData;
 import universidadgruppo93.Entidades.Alumno;
 
-/**
- *
- * @author Gabi
- */
 public class MenuGestionDeAlumnos extends javax.swing.JInternalFrame {
     private AlumnoData aluData=new AlumnoData();
     private Alumno alumnoActual=null;
